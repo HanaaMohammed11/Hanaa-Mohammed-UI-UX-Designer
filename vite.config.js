07a2a9f97af95d880c8170dbe36abba2cd29cb29
HanaaMohammed11 <hanaamohammed840@gmail.com>
2026-08-25 06:30:07 +0300

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/Hanaa-Mohammed-UI-UX-Designer/',
-  plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: false,
-      navigationNotifier: false,
-      analyticsTracker: false, 
-      visualEditAgent: false  
-    }),
-    react(),
-  ]
+  plugins: [react()],
 });
