4ccae7b660ba17e711aef5e5ec16bc23746e5b29
HanaaMohammed11 <hanaamohammed840@gmail.com>
2026-08-25 06:36:47 +0300

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   base: '/Hanaa-Mohammed-UI-UX-Designer/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
