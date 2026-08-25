6c4b4d015e3bdfaf37d423ef4ed5c8f33c282081
HanaaMohammed11 <hanaamohammed840@gmail.com>
2026-08-25 15:15:58 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster"
+import { TooltipProvider } from "@/components/ui/tooltip"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
@@ -9,14 +10,16 @@ import Home from './pages/Home';
 function App() {
   return (
     <QueryClientProvider client={queryClientInstance}>
-      <Router>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>
-      <Toaster />
+      <TooltipProvider delayDuration={0}>
+        <Router>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Router>
+        <Toaster />
+      </TooltipProvider>
     </QueryClientProvider>
   )
 }
