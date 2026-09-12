630a4a087060ab5893280a8209b954e941d3c6de
HanaaMohammed11 <hanaamohammed840@gmail.com>
2026-09-12 13:22:13 +0300

    add

--- src/components/ui/tooltip.jsx
+++ src/components/ui/tooltip.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import * as TooltipPrimitive from "@radix-ui/react-tooltip"
+import * * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
 import { cn } from "@/lib/utils"
 
@@ -19,7 +19,10 @@ const TooltipContent = React.forwardRef(({ className, side = "top", align = "cen
       align={align}
       sideOffset={sideOffset}
       className={cn(
-        "z-[9999] overflow-hidden rounded-md border-2 border-ink bg-yellow px-2 py-1 font-mono text-[10px] font-bold uppercase text-ink shadow-solid animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
+        "z-[9999] overflow-hidden rounded-md border-2 border-ink bg-yellow px-2 py-1 font-mono text-[10px] font-bold uppercase text-ink shadow-solid",
+        "animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
+
+        "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         className
       )}
       {...props}
